b32d80c86dfcbb70a31f1e97f3f9f85b0816c3ed
Elizaveta <eliz.vitebsk@gmail.com>
2019-11-08 14:58:06 +0300

    methods first version

---
+++ WcfPlaneTicketService/WcfPlaneTicketService/IPlaneTicketService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfPlaneTicketService
+{
+    [ServiceContract]
+    public interface IPlaneTicketService
+    {
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "/flights/{userId}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]        
+        List<Route> getUserFlightsInfo(string userId); // without date and price 
+
+        [OperationContract]  
+        [WebInvoke(
+            Method = "GET",
+            UriTemplate = "/flights/{userId}/details",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]       
+        List<Route> getFullUserFlightsInfo(string userId); // with date and price      
+
+        [OperationContract]  //?? что из клиента получаю? parameters - from + where + date + time + price ??
+        [WebInvoke(Method = "POST", UriTemplate = "/addFlight/{userId}/{parameters}")]
+        void addFlight(string userId, string parameters);
+
+        [OperationContract]  //?? что из клиента получаю? parameters - from + where + date + time + price ??
+        [WebInvoke(Method = "POST", UriTemplate = "/updateFlight/{userId}/{userFlightId}/{parameters}")]
+        void updateFlight(string userId, string userFlightId, string parameters);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/deleteFlight/{userId}/{userFlightId}")]
+        void deleteFlight(string userId, string userFlightId);
+    }
+
+    public class Route
+    {
+        public string routeId { get; set; }
+        public string routeFrom { get; set; }
+        public string routeWhere { get; set; }
+        public string routeDate { get; set; }
+        public string routeTime { get; set; }
+        public string routePrice { get; set; }
+    }
+
+    public class UserFlight
+    {
+        public string userFlightRouteId { get; set; }
+        public string userFlightUserId { get; set; }
+    }
+
+    public class User
+    {
+        public string userId { get; set; }
+        public string userEmail { get; set; }
+        public string userFirstName { get; set; }
+        public string userSecondName { get; set; }
+    }
+}
---
+++ WcfPlaneTicketService/WcfPlaneTicketService/PlaneTicketService.svc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfPlaneTicketService
+{
+    public class PlaneTicketService : IPlaneTicketService
+    {
+        public List<Route> getUserFlightsInfo(string userId)
+        {
+            List<Route> resRoutes = new List<Route>();
+            // by userId from UserFlight get routeId, for each routeId from Route get info(without date and price)
+
+            // test
+            Route route = new Route();
+            route.routeId = 1.ToString();
+            route.routeFrom = "Minsk";
+            route.routeWhere = "London";
+            route.routeDate = "01.01.2020";
+            //route.routeTime = "00:00";
+            //route.routePrice = "30";
+            resRoutes.Add(route);
+            return resRoutes;
+        }
+
+        public List<Route> getFullUserFlightsInfo(string userId)
+        {
+            List<Route> resRoutes = new List<Route>();
+            // by userId from UserFlight get routeId, for each routeId from Route get info(with date and price)
+            return resRoutes;
+        }
+
+        public void addFlight(string userId, string parameters)
+        {
+
+        }
+
+        public void updateFlight(string userId, string userFlightId, string parameters)
+        {
+
+        }
+
+        public void deleteFlight(string userId, string userFlightId)
+        {
+
+        }
+    }
+}
