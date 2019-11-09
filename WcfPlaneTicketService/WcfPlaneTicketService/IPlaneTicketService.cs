047645dc75fd6593dff14f2e2a0648616f39b592
Elizaveta <eliz.vitebsk@gmail.com>
2019-11-09 03:03:48 +0300

    datacontract, getUser, web.config

--- WcfPlaneTicketService/WcfPlaneTicketService/IPlaneTicketService.cs
+++ WcfPlaneTicketService/WcfPlaneTicketService/IPlaneTicketService.cs
@@ -14,10 +14,18 @@ namespace WcfPlaneTicketService
         [OperationContract]
         [WebInvoke(
             Method = "GET",
+            UriTemplate = "/{userId}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        User getUser(string userId); // userId = Email
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET",
             UriTemplate = "/flights/{userId}",
             RequestFormat = WebMessageFormat.Json,
             ResponseFormat = WebMessageFormat.Json)]        
-        List<Route> getUserFlightsInfo(string userId); // without date and price 
+        List<Route> getUserFlightsInfo(string userId); // without time and price 
 
         [OperationContract]  
         [WebInvoke(
@@ -25,7 +33,7 @@ namespace WcfPlaneTicketService
             UriTemplate = "/flights/{userId}/details",
             RequestFormat = WebMessageFormat.Json,
             ResponseFormat = WebMessageFormat.Json)]       
-        List<Route> getFullUserFlightsInfo(string userId); // with date and price      
+        List<Route> getFullUserFlightsInfo(string userId); // with time and price      
 
         [OperationContract]  //?? что из клиента получаю? parameters - from + where + date + time + price ??
         [WebInvoke(Method = "POST", UriTemplate = "/addFlight/{userId}/{parameters}")]
@@ -38,29 +46,53 @@ namespace WcfPlaneTicketService
         [OperationContract]
         [WebInvoke(Method = "POST", UriTemplate = "/deleteFlight/{userId}/{userFlightId}")]
         void deleteFlight(string userId, string userFlightId);
+
+        void setToken(string methodName, string tokenValue);
+        // methods which gets payment token
     }
 
+    public class Token
+    {
+        public string methodName { get; set; }
+        public string tokenValue { get; set; }
+    }
+
+    [DataContract]
     public class Route
     {
+        [DataMember]
         public string routeId { get; set; }
+        [DataMember]
         public string routeFrom { get; set; }
+        [DataMember]
         public string routeWhere { get; set; }
+        [DataMember]
         public string routeDate { get; set; }
+        [DataMember]
         public string routeTime { get; set; }
+        [DataMember]
         public string routePrice { get; set; }
     }
 
+    [DataContract]
     public class UserFlight
     {
+        [DataMember]
         public string userFlightRouteId { get; set; }
+        [DataMember]
         public string userFlightUserId { get; set; }
     }
 
+    [DataContract]
     public class User
     {
-        public string userId { get; set; }
-        public string userEmail { get; set; }
+        [DataMember]
+        public string userId { get; set; }   // Email = Id
+        //[DataMember]
+        //public string userEmail { get; set; }
+        [DataMember]
         public string userFirstName { get; set; }
+        [DataMember]
         public string userSecondName { get; set; }
     }
 }
